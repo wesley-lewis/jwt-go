d3d47bdafd04f79baee8c0ae63ecfd6c9eee08db
wesley-lewis <lewis.wes.23@gmail.com>
2023-06-19 12:45:25 +0530

    Resolved export bug in User struct

--- models/userModel.go
+++ models/userModel.go
@@ -8,15 +8,15 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson:"_id"`
-	firstName    *string            `json:"first_name" validate:"required, min=2, max=100`
-	lastName     *string            `json:"last_name" validate:"required, min=2, max=100"`
-	password     *string            `json:"password" validate:"required, min=10, max=100"`
-	email        *string            `json:"email" validate:"email, required"`
-	phone        *string            `json:"phone" validate:"required"`
-	token        *string            `json:"token"`
-	user_type    *string            `json:"user_type" validate:"required, eq=ADMIN|wq=USER"`
-	refreshToken *string            `json:"refresh_token"`
-	createdAt    time.Time          `json:"created_at"`
-	updatedAt    time.Time          `json:"updated_at"`
-	userID       *string            `json:"user_id"`
+	FirstName    *string            `json:"first_name" validate:"required, min=2, max=100`
+	LastName     *string            `json:"last_name" validate:"required, min=2, max=100"`
+	Password     *string            `json:"password" validate:"required, min=10, max=100"`
+	Email        *string            `json:"email" validate:"email, required"`
+	Phone        *string            `json:"phone" validate:"required"`
+	Token        *string            `json:"token"`
+	User_type    *string            `json:"user_type" validate:"required, eq=ADMIN|wq=USER"`
+	RefreshToken *string            `json:"refresh_token"`
+	CreatedAt    time.Time          `json:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at"`
+	UserID       *string            `json:"user_id"`
 }
