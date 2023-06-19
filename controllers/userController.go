7f180680259c93c48b43793c65ab5b69d860d744
wesley-lewis <lewis.wes.23@gmail.com>
2023-06-19 12:23:12 +0530

    GetUser route completed

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,64 @@
 package controllers
+
+import (
+	"context"
+	helper "jwt-go/helpers"
+	"jwt-go/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/wesley-lewis/jwt-go/database"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword() {
+
+}
+
+func VerifyPassword() {
+
+}
+
+func Signup() {
+
+}
+
+func Login() {
+
+}
+
+func GetUsers() {
+
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	}
+}
